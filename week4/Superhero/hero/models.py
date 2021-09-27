a7f5a1d15debd64e72ce29c6afa3b64fa4c4c513
TouchsadaJanOn <tjcomputer13@gmail.com>
2021-09-27 14:33:29 -0600

    addition

--- week4/Superhero/hero/models.py
+++ week4/Superhero/hero/models.py
@@ -3,10 +3,11 @@
 # Create your models here.
 from django.db import models
 
+
 class Hero (models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     image = models.CharField(max_length=200)
 
     def __str__(self):
-        return f'{self.name}'#', Description: {self.description}'
+        return f'{self.name}'  # ', Description: {self.description}'
--- week5/Bookbuilder/book/views.py
+++ week5/Bookbuilder/book/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+#from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
