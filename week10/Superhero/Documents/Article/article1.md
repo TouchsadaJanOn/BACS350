2f8c69cd4a6030498c16782178b1a034769816cb
TouchsadaJanOn <tjcomputer13@gmail.com>
2021-10-31 20:52:13 -0600

    continue working on theme, blocks, navbar, header, footer,
    improving article views and urls

---
+++ week10/Superhero/Documents/Article/article1.md
@@ -0,0 +1,10 @@
+# 1 - The Superhero Struggle
+
+## Spiderman, Batman, Aguaman, Deadpool, and Captain America
+
+The struggle is real when all the superheroes are in a fight. Spiderman bought a hot chai without asking other
+superheroes that they want one. He is being neglected.
+
+### The Discussion
+
+Batman, Aguaman, Deadpool, and Captain America will now fight Spiderman for the Chai Latte
---
+++ week10/Superhero/hero/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.urls.base import reverse_lazy
+# Create your models here.
+
+
+class Superhero(models.Model):
+
+    name = models.CharField(max_length=20)
+    identity = models.CharField(max_length=20)
+    description = models.TextField(default="none")
+    strength = models.CharField(max_length=100, default="none")
+    weakness = models.CharField(max_length=100, default="none")
+    image = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.name} who is {self.identity}'
+
+    def get_absolute_url(self):
+        return reverse_lazy('hero_details', args=[str(self.id)])
+
+
+class Article(models.Model):
+    # pointer to Superhero object
+    hero = models.CharField(max_length=20, default='none')
+    order = models.IntegerField()  # superhero order
+    # title text of the superhero
+    title = models.CharField(max_length=200)
+    markdown = models.TextField()  # markdown text
+    html = models.TextField()
+
+    def export_record(self):
+        return [self.hero, self.order, self.title]
+
+    @staticmethod
+    def import_record(values):
+        c = Article.objects.get_or_create(hero=values[0], order=values[1])[0]
+        c.title = values[2]
+        c.save()
+
+    def __str__(self):
+        return f'{self.hero.title} - {self.order} - {self.title}'
